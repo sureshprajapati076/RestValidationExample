62d6bc1aa74d1f4263b0d869bcf992f8f705a48c
sureshprajapati076 <sureshprajapati076@gmail.com>
2021-07-04 16:18:31 -0700

    add

--- src/main/java/com/example/demo/domain/Person.java
+++ src/main/java/com/example/demo/domain/Person.java
@@ -1,11 +1,28 @@
+package com.example.demo.domain;
 
-/*
-Writing in branch dwm-1234
+import com.example.demo.validator.PersonValidate;
+import com.fasterxml.jackson.annotation.JsonAlias;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@PersonValidate
+public class Person {
+
+    private String firstName;
+
+    private String lastName;
 
 
-Written by dwm-new
- */
+    private int age;
+    @JsonAlias("isPoliticallyExposed")
+    private boolean isPoliticallyExposed;
+    @JsonAlias("isSeniorExecutive")
+    private boolean isSeniorExecutive;
 
-This is written in dwm-1234
-git added from false
+    private String companyName;
 
+}
